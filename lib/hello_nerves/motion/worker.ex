4a855734f334a3c56221edb041582a418a45e115
Jacqui Manzi <jacquimanzi@gmail.com>
2019-08-23 15:25:59 -0700

    Code cleanup

--- lib/hello_nerves/motion/worker.ex
+++ lib/hello_nerves/motion/worker.ex
@@ -17,14 +17,12 @@ defmodule HelloNerves.Motion.Worker do
   @impl true
   def handle_info(
         :reconnect_port,
-        %{port: _port, count: count, stream_allowed: stream_allowed, ffmpeg_info: ffmpeg_info} =
-          state
+        state
       ) do
     Logger.info("we're spawning a new port...")
 
     with new_port when is_port(new_port) <- spawn_rtmp_port() do
-      {:noreply,
-       %{port: new_port, count: count, stream_allowed: stream_allowed, ffmpeg_info: ffmpeg_info}}
+      {:noreply, %{state | port: new_port}}
     else
       _ ->
         Process.send_after(self(), :reconnect_port, 10_000)
@@ -35,17 +33,17 @@ defmodule HelloNerves.Motion.Worker do
   @impl true
   def handle_info(
         :allow_streaming,
-        %{port: port, count: count, stream_allowed: _stream_allowed, ffmpeg_info: ffmpeg_info}
+        state
       ) do
     Logger.info("Enabling streaming")
 
-    {:noreply, %{port: port, count: count, stream_allowed: true, ffmpeg_info: ffmpeg_info}}
+    {:noreply, %{state | stream_allowed: true}}
   end
 
   @impl true
   def handle_info(
         :restart_picam,
-        %{port: port, count: count, stream_allowed: _stream_allowed, ffmpeg_info: ffmpeg_info}
+        state = %{port: port}
       ) do
     Logger.info("Restarting the Picam port process and stopping our stream")
 
@@ -55,14 +53,13 @@ defmodule HelloNerves.Motion.Worker do
     pid = Process.whereis(Picam.Camera)
 
     Process.send(pid, :reconnect_port, [])
-    {:noreply, %{port: port, count: count, stream_allowed: false, ffmpeg_info: ffmpeg_info}}
+    {:noreply, %{state | stream_allowed: false}}
   end
 
   @impl true
   def handle_info(
         :spawn_rtmp_port,
-        %{port: _port, count: count, stream_allowed: _stream_allowed, ffmpeg_info: ffmpeg_info} =
-          state
+        state
       ) do
     Logger.info("attempting to open rtmp port")
 
@@ -72,7 +69,7 @@ defmodule HelloNerves.Motion.Worker do
          {:ok, _} <-
            ExTwilio.Message.create(to: target_number, from: twilio_number_you_own, body: body) do
       rtmp_port = spawn_rtmp_port()
-      {:noreply, %{port: rtmp_port, count: count, stream_allowed: true, ffmpeg_info: ffmpeg_info}}
+      {:noreply, %{state | port: rtmp_port, stream_allowed: true}}
     else
       _ -> {:noreply, state}
     end
@@ -96,12 +93,13 @@ defmodule HelloNerves.Motion.Worker do
   end
 
   @impl true
-  def handle_cast({:detect_motion, image}, %{
-        port: port,
-        count: previous_count,
-        stream_allowed: stream_allowed,
-        ffmpeg_info: ffmpeg_info
-      }) do
+  def handle_cast(
+        {:detect_motion, image},
+        state = %{
+          count: previous_count,
+          stream_allowed: stream_allowed
+        }
+      ) do
     count = get_image_binary_sum(image)
     percentage = previous_count * @motion_sensitivity
 
@@ -112,8 +110,7 @@ defmodule HelloNerves.Motion.Worker do
       if stream_allowed, do: kill_picam()
     end
 
-    {:noreply,
-     %{port: port, count: count, stream_allowed: stream_allowed, ffmpeg_info: ffmpeg_info}}
+    {:noreply, %{state | count: count}}
   end
 
   defp kill_picam() do
@@ -142,7 +139,7 @@ defmodule HelloNerves.Motion.Worker do
   end
 
   defp spawn_rtmp_port() do
-    executable = System.find_executable("sh")
+    executable = System.find_executable("ffmpeg")
 
     Port.open({:spawn_executable, executable}, [
       :stderr_to_stdout,
@@ -151,10 +148,36 @@ defmodule HelloNerves.Motion.Worker do
       :binary,
       {:args,
        [
-         "-c",
-         "raspivid -o - -t 0 --mode 1 -a 1036 -fps 30 -b 3000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -r 30 -i - -vcodec copy -acodec aac -ab 64k -r 30 -g 50 -strict experimental -f flv #{
-           Application.get_env(:mux, :stream_url)
-         }/#{Application.get_env(:mux, :stream_key)}"
+         "-f",
+         "video4linux2",
+         "-framerate",
+         "30",
+         "-input_format",
+         "nv12",
+         "-video_size",
+         "640x480",
+         "-i",
+         "/dev/video0",
+         "-b:a",
+         "64k",
+         "-c:v",
+         "libx264",
+         "-preset",
+         "ultrafast",
+         "-pix_fmt",
+         "yuv420p",
+         "-b:v",
+         "3000k",
+         "-g",
+         "50",
+         "-refs",
+         "3",
+         "-bf",
+         "0",
+         "-an",
+         "-f",
+         "flv",
+         "#{Application.get_env(:mux, :stream_url)}/#{Application.get_env(:mux, :stream_key)}"
        ]}
     ])
   end
